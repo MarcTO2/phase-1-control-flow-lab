b41146053f903eee946f9eb354d862148c6e7b11
Absalom <mrclabsalom@gmail.com>
2023-06-20 09:53:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
   // Write your code here!
+  function scuberGreetingForFeet(freeride){
+    if(freeride <= 2000){
+      return "This one is on me!"
+    }else if (freeride >= 2001 && freeride < 2501){
+      return 'I will gladly take your thirty bucks.'
+    }else (freeride > 2500);{
+      return 'No can do.'
+    }
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city == 'NYC' ? "Ok, sounds good." : "No go.")
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
-}
+  switch(generous){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+      return 'Thank you.'
+      case 'thanks for everything':
+        return 'Bye.'
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
